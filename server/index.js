d46f7783dc149e27d6c743938ac4ce54fa5bc8f6
unknown <seneryavuzberk@gmail.com>
2025-07-23 17:34:13 +0200

    Initial project structure with Next.js frontend and Express backend

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
